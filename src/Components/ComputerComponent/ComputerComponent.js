164b3d47406661692d57ade78770f7c689d2fcb0
tanvirnpm <tanvirnpm@gmail.com>
2021-09-26 09:44:05 +0600

    first commit

---
+++ src/Components/ComputerComponent/ComputerComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComputerComponent = () => {
+    return (
+        <div>
+            <h1>Computer Components</h1>
+        </div>
+    );
+};
+
+export default ComputerComponent;
